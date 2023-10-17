45423da16d322cd71b8cfa38f72dd372ede64daf
Bryan Mico V. Baril <Imbryanmicobaril@gmail.com>
2023-10-17 22:35:14 +0800

    Update/Delete Button and Status symbol

--- src/sharedData/studentsData.ts
+++ src/sharedData/studentsData.ts
@@ -3,43 +3,50 @@ export const students_Data = [
         studentImg: "assets/img1.jpg",
         studentName: "Bryan Mico V. Baril",
         Course: "Bachelor of Science in Information Techology",
-        studentID: 10000000
+        studentID: 10000000,
+        activeStatus: true
     },
     {
         studentImg: "assets/img2.jpg",
         studentName: "Emma Johnson",
         Course: "Bachelor of Science in Biology",
-        studentID: 11000000
+        studentID: 11000000,
+        activeStatus: false
     },
     {
         studentImg: "assets/img3.jpg",
         studentName: "Liam Smith",
         Course: "Bachelor of Arts in Psychology",
-        studentID: 11100000
+        studentID: 11100000,
+        activeStatus: false
     },
     {
         studentImg: "assets/img4.jpg",
         studentName: "Sophia Davis",
         Course: "Bachelor of Social Work",
-        studentID: 11110000
+        studentID: 11110000,
+        activeStatus: false
     },
     {
         studentImg: "assets/img5.jpg",
         studentName: "Ethan Taylor",
         Course: "Bachelor of Engineering in Mechanical Engineering",
-        studentID: 11111000
+        studentID: 11111000,
+        activeStatus: false
     },
     {
         studentImg: "assets/img6.jpg",
         studentName: "Isabella White",
         Course: "Bachelor of Fine Arts in Graphic Design",
-        studentID: 11111110
+        studentID: 11111110,
+        activeStatus: false
     },
     {
         studentImg: "assets/img7.jpg",
         studentName: "Olivia Brown",
         Course: "Bachelor of Science in Nursing",
-        studentID: 12110000
+        studentID: 12110000,
+        activeStatus: false
     },
 
 ]
--- src/sharedData/teachersData.ts
+++ src/sharedData/teachersData.ts
@@ -3,18 +3,21 @@ export const teachers_Data = [
         teacherImg: "assets/t1.jpg",
         teacherName: "David Smith",
         Subject: "Computer Architecture",
-        teacherID: 20000000
+        teacherID: 20000000,
+        activeStatus: false
     },
     {
         teacherImg: "assets/t2.jpg",
         teacherName: "John Turner",
         Subject: "Cognitive Psychology",
-        teacherID: 21000000
+        teacherID: 21000000,
+        activeStatus: false
     },
     {
         teacherImg: "assets/t3.jpg",
         teacherName: "Robert White",
         Subject: " Entrepreneurship",
-        teacherID: 21100000
+        teacherID: 21100000,
+        activeStatus: false
     }
 ]
